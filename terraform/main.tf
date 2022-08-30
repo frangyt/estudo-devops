2651847ea7ff6a2cb977d4f63862fbd83df869c0
Frangit <leandro.frangit@gmail.com>
2022-08-30 15:53:21 -0300

    primeira aula terraform

---
+++ terraform/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {  
+  region = "us-east-1"
+}
+
+resource "aws_instance" "dev" {
+  count = 3
+  ami = "ami-052efd3df9dad4825"
+  instance_type = "t2.micro"
+  key_name = "terraform-aws"
+  tags = {
+    Name = "dev${count.index}"
+  }
+}
